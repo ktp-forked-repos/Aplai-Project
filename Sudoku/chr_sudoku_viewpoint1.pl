e92338d99405d0f9cc0ac677c45b3c6e86d22880
SanderGn <sandergeijsen@hotmail.com>
2015-05-28 21:41:02 +0200

    CHR Sudoku aanmaken

--- Sudoku/chr_sudoku_viewpoint1.pl
+++ Sudoku/chr_sudoku_viewpoint1.pl
@@ -4,42 +4,48 @@
 
 :- chr_option(debug,on).
 
-:- chr_type list(T) ---> [] ; [T | list(T)].
+% data types bepalen.
 
-:- chr_constraint makeDomainRow(?list(int)).
+% Definitie van het data type lijst.
+:- chr_type list(T) ---> [] ; [T | list(T)].
+% natural is positieve integer.
+:- chr_type xvalue == natural.
+:- chr_type yvalue == natural.
+:- chr_type position ---> (xvalue,yvalue).
+% De waarde van een paar (element) kan een natural zijn of een domein(lijst van naturals)
+:- chr_type number ---> natural ; [natural | list(natural)].
+
+% + wil zeggen dat de variabele altijd grounded is. - wil zeggen nooit. ? wil zeggen allebei en kan veranderen over tijd.
+:- chr_constraint element(+position,+number),printBoard(+natural,+natural).
 
 solve(SudokuBoard) :- 
-        makeDomainBoard(SudokuBoard),
-        constraintsBoard(SudokuBoard),
-        printBoard(SudokuBoard).       
+        makeDomainBoard(SudokuBoard,1).
+        %printBoard(1,1).
 
-makeDomainBoard([]).
-makeDomainBoard([Row|RestBoard]) :-
-        makeDomainRow(Row),
-        makeDomainBoard(RestBoard).
-        
-makeDomainRow([]).
-makeDomainRow([Element|RestRow]) :-
-        generateDomain(1,9,Domain),
-        Element in Domain,
-        makeDomainRow(RestRow).        
+makeDomainBoard([],_).
+makeDomainBoard([Row|RestBoard],RowI) :-
+        makeRowDomain(Row,RowI,1),
+        RowI1 is RowI + 1, 
+        makeDomainBoard(RestBoard, RowI1).
 
-generateDomain(N,N,[N]).
-generateDomain(M,N,[M|T]):-
-        M < N,
-        M1 is M+1,
-        generateDomain(M1,N,T).
+makeRowDomain([],_,_).
+makeRowDomain([Element|RestRow],RowI,RowJ) :-
+        nonvar(Element),
+        element((RowI,RowJ), [Element]),
+        RowJ1 is RowJ + 1,
+        makeRowDomain(RestRow,RowI,RowJ1).
+makeRowDomain([Element|RestRow],RowI,RowJ) :-
+        var(Element),
+        element((RowI,RowJ), [1,2,3,4,5,6,7,8,9]),
+        RowJ1 is RowJ + 1,
+        makeRowDomain(RestRow,RowI,RowJ1).
 
-constraintsBoard(_).
+rowConstraints @ element((X,Y1),[A]), element((X,Y2),[A]) <=> Y1 == Y2, element((X,Y1),[A]).
+%printBoard(9,L), element((9,L),[A]) <=> writeln(A).
+%printBoard(K,9), element((K,9),[A]) <=> writeln(A), K1 is K + 1, printBoard(K1,1).
+%printBoard(K,L), element((K,L),[A]) <=> write(A), Y is L + 1, printBoard(K,Y).
 
-printBoard([]).
-printBoard([Row|RestBoard]) :-
-        printRow(Row),
 
-        writeln(" "),
-        printBoard(RestBoard).
 
-printRow([]).
-printRow([X|Rest]) :-
-        write(X),
-        printRow(Rest).
+
+
--- Sudoku/test.pl
+++ Sudoku/test.pl
@@ -1,6 +1,10 @@
 :- use_module(library(chr)).
 
-:- chr_constraint test/2.
+:- chr_constraint piggy/1,generate/1,value/1.
 
-test(9,List) <=> List :: 1:9.
-test(_,List) <=> List is 1+1.
+piggy(I), piggy(J) <=> K is I + J, piggy(K).
+
+generate(N) <=> N > 0 | value(N),
+                        M is N - 1, generate(M).
+
+%value(I),value(J) <=> K is I + J, value(K).
