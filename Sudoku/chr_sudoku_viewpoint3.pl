1df429c0d9744c607e4f03736918b55cc59d6bb2
Sander Geijsen <r0304675@pittem.cs.kotnet.kuleuven.be>
2015-05-31 18:24:23 +0200

    CHR tweede viewpoint doet kut

--- Sudoku/chr_sudoku_viewpoint3.pl
+++ Sudoku/chr_sudoku_viewpoint3.pl
@@ -1,5 +1,8 @@
-:- module(chr_sudoku_viewpoint1,[]).
+:- module(chr_sudoku_viewpoint3,[]).
 :- use_module(library(chr)).
+:- use_module(library(lists)).
+
+:- chr_option(optimize,full).
 
 :- chr_type list(T) ---> [] ; [T | list(T)].
 :- chr_type xvalue == natural.
@@ -13,14 +16,15 @@
 :- chr_constraint eliminatePos/0.
 :- chr_constraint checkColCount(+matrixvalue,+position,+natural).
 :- chr_constraint element(+matrixvalue,+position,+natural).
-:- chr_constraint printBoard(+natural,+natural).
+:- chr_constraint printBoard(+natural,+natural,+natural).
 :- chr_constraint findSmallestDomain(+natural).
+:- chr_constraint matrixSom(+natural,+position, +natural).
+
 
 solve(SudokuBoard) :-
         makeBoardDomain(SudokuBoard,1),
-        eliminatePos.
-        %choiceNumber.
-        %printBoard(1,1).
+        eliminatePos,
+        printBoard(1,1,1).
 
 makeBoardDomain([],_).
 makeBoardDomain([Row|Board],RowNumber) :-
@@ -53,43 +57,48 @@ makeNonVarElement(Element,RowNumber,ColumnNumber,MatrixCounter) :-
 
 makeVarElement(_,_,10).
 makeVarElement(RowNumber, ColumnNumber, MatrixNumber) :-
-        posElement(MatrixNumber,(RowNumber,ColumnNumber), [0,1]),
+        posElement(MatrixNumber,(RowNumber,ColumnNumber), [1,0]),
         NextMatrixNumber is MatrixNumber + 1,
         makeVarElement(RowNumber,ColumnNumber,NextMatrixNumber).
+
+makeMatrixValues(10).
+makeMatrixValues(N) :-
+        matrixSom(N,0),
+        NN is N + 1,
+        makeMatrixValues(NN).
 
 checkBox(X1,X2,Y1,Y2) :-
         Block1 is (3*((X1-1) // 3)) + ((Y1-1) // 3) + 1,
         Block2 is (3*((X2-1) // 3)) + ((Y2-1) // 3) + 1,
         Block1 == Block2.
 
-rowconstraint @ element(A,(X,Y1),1), element(A,(X,Y2),1) <=> Y1 \= Y2 | false.
+posElement(A,(X,Y),[Value]) <=> element(A,(X,Y),Value).
 
-columnConstraint @ element(A,(X1,Y),1), element(A,(X2,Y),1) <=> X1 \= X2 | false.
+rowconstraint @ element(A,(X,Y1),1), element(A,(X,Y2),1) <=> Y1 == Y2.
+
+columnConstraint @ element(A,(X1,Y),1), element(A,(X2,Y),1) <=> X1 == X2.
 
 blockConstraint @ element(A,(X1,Y1),1), element(A,(X2,Y2),1) <=> checkBox(X1,X2,Y1,Y2) | false.
 
-elementConstraint @ element(A,(X,Y),1), element(B,(X,Y),1) <=> A \= B | false.
-
+elementConstraint @ element(A,(X,Y),1), element(B,(X,Y),1) <=> A == B.
+               
 % zelfde matrix row
-eliminatePos, element(A,(X,Y2),AssignedValue) \ posElement(A,(X,Y1),ListPos) <=> Y1 \= Y2, select(AssignedValue,ListPos,NListPos) | member(P,NListPos),element(A,(X,Y1),P). 
+eliminatePos, element(A,(X,Y2),1) \ posElement(A,(X,Y1),ListPos) <=> Y1 \= Y2, select(1,ListPos,NListPos) | posElement(A,(X,Y1),NListPos). 
 
 % zelfde matrix kolom
-eliminatePos, element(A,(X1,Y),AssignedValue) \ posElement(A,(X2,Y),ListPos) <=> 
-        X1 \= X2, select(AssignedValue,ListPos,NListPos) | member(P,NListPos),element(A,(X2,Y),P).  
+eliminatePos, element(A,(X1,Y),1) \ posElement(A,(X2,Y),ListPos) <=> X1 \= X2, select(1,ListPos,NListPos) | posElement(A,(X2,Y),NListPos).  
 
 % zelfde matrix block
-eliminatePos, element(A,(X1,Y1),AssignedValue) \ posElement(A,(X2,Y2),ListPos) <=> 
-        checkBox(X1,X2,Y1,Y2), select(AssignedValue,ListPos,NListPos) | member(P,NListPos),element(A,(X2,Y2),P). 
+eliminatePos, element(A,(X1,Y1),1) \ posElement(A,(X2,Y2),ListPos) <=> checkBox(X1,X2,Y1,Y2), select(1,ListPos,NListPos) | posElement(A,(X2,Y2),NListPos). 
 
 % zelfde element alle matrices
-eliminatePos, element(A,(X,Y),AssignedValue) \ posElement(B,(X,Y),ListPos) <=> 
-        A \= B, select(AssignedValue,ListPos,NListPos) | member(P,NListPos),element(A,(X,Y),P). 
+eliminatePos, element(A,(X,Y),1) \ posElement(B,(X,Y),ListPos) <=> A \= B, select(1,ListPos,NListPos) | posElement(B,(X,Y),NListPos). 
 
-eliminatePos <=> findSmallestDomain(2).
+eliminatePos, posElement(A,(X,Y),PosList) <=> member(K,PosList), element(A,(X,Y),K),eliminatePos.
 
-findSmallestDomain(DomainLength), posElement(A,(X,Y),PosList) <=> length(PosList,LengthList), DomainLength == LengthList | member(K,PosList), element(A,(X,Y),K), eliminatePos.
+printBoard(9,9,9), element(9,(9,9),A) <=> writeln(A).
+printBoard(9,L,9), element(9,(L,9),A) <=> writeln(A), L1 is L + 1, printBoard(1,L1,1).
+printBoard(K,L,9), element(K,(L,9),A) <=> write(A + ""), K1 is K + 1, printBoard(K1,L,1).
+printBoard(K,L,Y), element(K,(L,Y),A) <=> write(A), Y1 is Y + 1, printBoard(K,L,Y1).
 
 
-%printBoard(9,9), element((9,9),A) <=> writeln(A).
-%printBoard(K,9), element((K,9),A) <=> writeln(A), K1 is K + 1, printBoard(K1,1).
-%printBoard(K,L), element((K,L),A) <=> write(A), Y is L + 1, printBoard(K,Y).
